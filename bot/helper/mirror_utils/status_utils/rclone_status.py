07b043a149c71d38bfc6cc8a94b1ec8fb268a065
TG-99 <tempgh99@proton.me>
2023-11-02 11:31:38 +0600

    rir18:mltb-main

---
+++ bot/helper/mirror_utils/status_utils/rclone_status.py
@@ -0,0 +1,45 @@
+import subprocess
+from bot.helper.ext_utils.bot_utils import MirrorStatus
+
+eng = subprocess.check_output(['rclone', 'version']).decode().split('\n')[0].strip()
+
+
+class RcloneStatus:
+    def __init__(self, obj, message, gid, status):
+        self.__obj = obj
+        self.__gid = gid
+        self.__status = status
+        self.message = message
+        self.engine = eng
+
+    def gid(self):
+        return self.__gid
+
+    def progress(self):
+        return self.__obj.percentage
+
+    def speed(self):
+        return self.__obj.speed
+
+    def name(self):
+        return self.__obj.name
+
+    def size(self):
+        return self.__obj.size
+
+    def eta(self):
+        return self.__obj.eta
+
+    def status(self):
+        if self.__status == 'dl':
+            return MirrorStatus.STATUS_DOWNLOADING
+        elif self.__status == 'up':
+            return MirrorStatus.STATUS_UPLOADING
+        else:
+            return MirrorStatus.STATUS_CLONING
+
+    def processed_bytes(self):
+        return self.__obj.transferred_size
+
+    def download(self):
+        return self.__obj
---
+++ bot/modules/cleandb.py
@@ -0,0 +1,45 @@
+#!/usr/bin/env python3
+from pyrogram.handlers import MessageHandler, CallbackQueryHandler
+from pyrogram.filters import command, regex
+from bot import bot, LOGGER, config_dict
+from bot.helper.telegram_helper.message_utils import sendMessage, editMessage
+from bot.helper.telegram_helper.filters import CustomFilters
+from bot.helper.telegram_helper.bot_commands import BotCommands
+from bot.helper.telegram_helper.button_build import ButtonMaker
+from bot.helper.ext_utils.db_handler import DbManger
+
+async def cleandb(client, message):
+    buttons = ButtonMaker()
+    buttons.ibutton("Drop Database", f"cdb database")
+    buttons.ibutton("All Bot Config", f"cdb allconfig")
+    buttons.ibutton("This Bot Config", f"cdb config")
+    buttons.ibutton("Close", f"cdb close")
+    button = buttons.build_menu(2)
+    await sendMessage(message, 'Clear Database Menu', button)
+
+async def cleandb_update(client, query):
+    message = query.message
+    data = query.data.split()
+    if data[1] == 'database':
+        await query.answer()
+        await DbManger().clean_database()
+        await editMessage(message, "All Database removed!")
+        LOGGER.info("All Database removes!")
+    elif data[1] == 'allconfig':
+        await query.answer()
+        await DbManger().clean_allconfig()
+        await editMessage(message, "All Config removed!")
+        LOGGER.info("All Config removes!")
+    elif data[1] == 'config':
+        await query.answer()
+        await DbManger().clean_config(config_dict)
+        await editMessage(message, "This bot Config removed!")
+        LOGGER.info("This bot Config removes!")
+    else:
+        await query.answer()
+        await message.reply_to_message.delete()
+        await message.delete()
+
+
+bot.add_handler(MessageHandler(cleandb, filters=command(BotCommands.CleanDBCommand) & CustomFilters.sudo))
+bot.add_handler(CallbackQueryHandler(cleandb_update, filters=regex("^cdb")))
