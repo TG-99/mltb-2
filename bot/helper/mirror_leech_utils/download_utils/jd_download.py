632231e092835e6239255169253a81c227f29a9e
TG-99 <tempgh99@proton.me>
2024-04-16 14:13:00 +0600

    Re-sync

--- bot/helper/mirror_leech_utils/download_utils/jd_download.py
+++ bot/helper/mirror_leech_utils/download_utils/jd_download.py
@@ -101,7 +101,11 @@ async def add_jd_download(listener, path):
             if not is_connected:
                 await listener.onDownloadError(jdownloader.error)
                 return
-            await jdownloader.connectToDevice()
+            jdownloader.boot()
+            isDeviceConnected = await jdownloader.connectToDevice()
+            if not isDeviceConnected:
+                await listener.onDownloadError(jdownloader.error)
+                return
 
         if not jd_downloads:
             await retry_function(jdownloader.device.linkgrabber.clear_list)
@@ -117,7 +121,9 @@ async def add_jd_download(listener, path):
             jdownloader.device.linkgrabber.query_packages, [{}]
         ):
             odl_list = [
-                od["uuid"] for od in odl if od["saveTo"].startswith("/root/Downloads/")
+                od["uuid"]
+                for od in odl
+                if od.get("saveTo", "").startswith("/root/Downloads/")
             ]
             if odl_list:
                 await retry_function(
@@ -292,7 +298,7 @@ async def add_jd_download(listener, path):
         package_ids=online_packages,
     )
 
-    await sleep(0.5)
+    await sleep(1)
 
     download_packages = await retry_function(
         jdownloader.device.downloads.query_packages,
