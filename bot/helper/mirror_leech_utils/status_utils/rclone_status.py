8e14d74ef1aadc42d093e4966bb15dcd0e98d7a8
TG-99 <tempgh99@proton.me>
2024-08-13 12:06:24 +0600

    rir18:mltb-without_NZBD

---
+++ bot/helper/mirror_leech_utils/status_utils/rclone_status.py
@@ -0,0 +1,45 @@
+from bot.helper.ext_utils.status_utils import MirrorStatus
+import subprocess
+
+eng = subprocess.check_output(['rclone', 'version']).decode().split('\n')[0].strip()
+
+
+class RcloneStatus:
+    def __init__(self, listener, obj, gid, status):
+        self._obj = obj
+        self._gid = gid
+        self._status = status
+        self.listener = listener
+        self.engine = eng
+
+    def gid(self):
+        return self._gid
+
+    def progress(self):
+        return self._obj.percentage
+
+    def speed(self):
+        return self._obj.speed
+
+    def name(self):
+        return self.listener.name
+
+    def size(self):
+        return self._obj.size
+
+    def eta(self):
+        return self._obj.eta
+
+    def status(self):
+        if self._status == "dl":
+            return MirrorStatus.STATUS_DOWNLOADING
+        elif self._status == "up":
+            return MirrorStatus.STATUS_UPLOADING
+        else:
+            return MirrorStatus.STATUS_CLONING
+
+    def processed_bytes(self):
+        return self._obj.transferred_size
+
+    def task(self):
+        return self._obj
